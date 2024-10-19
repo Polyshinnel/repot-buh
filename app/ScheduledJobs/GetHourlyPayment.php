7def6f357a051dfe17a172500a009757589a170f
Polyshinnel <polyshinnel@gmail.com>
2024-10-19 15:12:24 +0300

    fix err

--- app/ScheduledJobs/GetHourlyPayment.php
+++ app/ScheduledJobs/GetHourlyPayment.php
@@ -50,7 +50,6 @@ class GetHourlyPayment
                                         'commission' => $commission
                                     ];
                                 }
-
                             }
 
                         }
