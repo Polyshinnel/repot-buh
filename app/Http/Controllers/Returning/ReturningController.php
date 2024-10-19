85d565e35e958d8206e9080d9d175e4f3084eb79
Polyshinnel <polyshinnel@gmail.com>
2024-10-19 14:12:23 +0300

    add returning info

--- app/Http/Controllers/Returning/ReturningController.php
+++ app/Http/Controllers/Returning/ReturningController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Returning;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Utils\TimeController;
+use App\Models\Payment;
+use App\Models\Returning;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class ReturningController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $user = User::find(session('user_id'));
         $pageTitle = 'Сводка возвратов';
@@ -24,14 +27,128 @@ class ReturningController extends Controller
                 'active' => true
             ],
         ];
+
+        $dateInfo = 'month';
+        if($request->query('date_info')) {
+            $dateInfo = $request->query('date_info');
+        }
+
+        $blockTitle = '';
+
+        //Фильтр сегодня
+        if($dateInfo == 'today') {
+            $date = date('Y-m-d');
+            $selectDate = $date.' '.'00:00:00';
+            $filter = [
+                ['payment_time', '>=', $selectDate]
+            ];
+            $returnings = Returning::where($filter)->orderBy('payment_time', 'DESC')->get();
+            $blockTitle = 'Возвраты за сегодня';
+        }
+
+        //Фильтр вчера
+        if($dateInfo == 'yesterday') {
+            $date = date('Y-m-d');
+            $selectDate = $date.' '.'00:00:00';
+            $selectedDateSec = strtotime($selectDate);
+            $yesterday = $selectedDateSec - (24 * 3600);
+            $yesterdayDate = date('Y-m-d', $yesterday);
+            $yesterdayDateStart = sprintf('%s 00:00:00', $yesterdayDate);
+            $yesterdayDateEnd = sprintf('%s 23:59:59', $yesterdayDate);
+
+            $returnings = Returning::whereBetween('payment_time', [$yesterdayDateStart, $yesterdayDateEnd])->orderBy('payment_time', 'DESC')->get();
+            $blockTitle = 'Возвраты за вчера';
+        }
+
+        if($dateInfo == 'interval') {
+            $timeController = new TimeController();
+            $blockTitle = 'Возвраты ';
+            $dateStart = '';
+            if($request->query('date_start')) {
+                $dateStart = $request->query('date_start');
+                $dateStart = $timeController->reformatDate($dateStart, 'en');
+                $dateStart = sprintf('%s 00:00:00', $dateStart);
+            }
+
+            $dateEnd = '';
+            if($request->query('date_end')) {
+                $dateEnd = $request->query('date_end');
+                $dateEnd = $timeController->reformatDate($dateEnd, 'en');
+                $dateEnd = sprintf('%s 23:59:59', $dateEnd);
+            }
+
+            //Если пуста дата начала
+            if($dateStart == '' && $dateEnd != '') {
+                $filter = [
+                    ['payment_time', '<=', $dateEnd]
+                ];
+                $returnings = Returning::where($filter)->orderBy('payment_time', 'DESC')->get();
+                $blockTitle .= 'по '. $dateEnd;
+            }
+
+            //Если пуста дата конца
+            if($dateStart != '' && $dateEnd == '') {
+                $filter = [
+                    ['payment_time', '>=', $dateStart]
+                ];
+                $returnings = Returning::where($filter)->orderBy('payment_time', 'DESC')->get();
+                $blockTitle .= 'c '. $dateStart;
+            }
+
+            //Если получен интервал
+            if($dateStart != '' && $dateEnd != '') {
+                $returnings = Returning::whereBetween('payment_time', [$dateStart, $dateEnd])->orderBy('payment_time', 'DESC')->get();
+                $blockTitle .= 'c '. $dateStart.' по '.$dateEnd;
+            }
+        }
+
+        //Фильтр по умолчанию
+        if($dateInfo == 'month') {
+            $dateStart = sprintf('%s-01 00:00:00', date('Y-m'));
+            $dateEnd = sprintf('%s 23:59:59', date('Y-m-t'));
+
+            $returnings = Returning::whereBetween('payment_time', [$dateStart, $dateEnd])->orderBy('payment_time', 'DESC')->get();
+            $blockTitle = sprintf('Возвраты с 01.%s по %s', date('m.Y'), date('t.m.Y'));
+        }
+
+
+
+
+        $formattedReturnings = [];
+
+        if(!$returnings->isEmpty()) {
+            foreach ($returnings as $returning) {
+                $siteInfo = $returning->site_info;
+                $orderId = $returning->order_id;
+                $orderArr = explode(' ', $orderId);
+                $orderName = $orderArr[1];
+                $link = 'Пусто';
+                if(!str_contains($orderName, 'beauty')) {
+                    $orderNum = preg_replace('/[^0-9]/', '', $orderName);
+                    $link = sprintf('%s/panel/?module=OrderAdmin&id=%s', $siteInfo->site_addr, $orderNum);
+                }
+
+                $formattedReturnings[] = [
+                    'payment_date' => $returning->payment_time,
+                    'payment_sum' => $returning->payment_sum,
+                    'site' => $siteInfo->site_addr,
+                    'order_id' => $returning->order_id,
+                    'payment_id' => $returning->payment_order_id,
+                    'link' => $link
+                ];
+            }
+        }
+
+
         return view(
             'pages.Returning.returning',
             [
                 'username' => $user->name,
                 'page_title' => $pageTitle,
                 'breadcrumbs' => $breadcrumbs,
-                'block_title' => 'Возвраты',
-                'link' => '/returning'
+                'block_title' => $blockTitle,
+                'link' => '/returning',
+                'returnings' => $formattedReturnings
             ]
         );
     }
--- resources/views/pages/Returning/returning.blade.php
+++ resources/views/pages/Returning/returning.blade.php
@@ -9,17 +9,61 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <table id="example1" class="table table-bordered table-striped">
+                    <div class="table-toolbar">
+                        <div class="filter-block d-flex justify-content-end">
+                            <button type="button" class="btn btn-outline-primary filter-btn">Фильтр</button>
+                        </div>
+
+                        <div class="filter-block-list justify-content-end mt-3 mb-5 d-none">
+                            <div class="row">
+                                <button type="button" class="btn btn-primary month">Месяц</button>
+                                <button type="button" class="btn btn-outline-primary today ml-2">Сегодня</button>
+                                <button type="button" class="btn btn-outline-primary yesterday ml-2">Вчера</button>
+                                <button type="button" class="btn btn-outline-primary interval ml-2">Интервал</button>
+                            </div>
+                        </div>
+                        <div class="interval-block d-none">
+                            <div class="row justify-content-end align-items-center">
+                                <div class="form-group">
+                                    <label for="date_start">Дата начала</label>
+                                    <input type="text" class="form-control" id="date_start">
+                                </div>
+                                <div class="form-group ml-2">
+                                    <label for="date_end">Дата окончания</label>
+                                    <input type="text" class="form-control" id="date_end">
+                                </div>
+                                <button type="button" class="btn btn-primary interval-accept ml-2 mt-3">Применить</button>
+                            </div>
+                        </div>
+                    </div>
+                    <table id="returning-table" class="table table-bordered table-striped">
                         <thead>
                             <tr>
                                 <th>Дата платежа</th>
                                 <th>Сумма платежа</th>
                                 <th>Сайт</th>
                                 <th>Id заказа</th>
+                                <th>Ссылка</th>
                             </tr>
                         </thead>
                         <tbody>
-
+                            @if($returnings)
+                                @foreach($returnings as $returning)
+                                    <tr>
+                                        <td>{{$returning['payment_date']}}</td>
+                                        <td>{{$returning['payment_sum']}}</td>
+                                        <td>{{$returning['site']}}</td>
+                                        <td>{{$returning['order_id']}}</td>
+                                        <td>
+                                            @if($returning['link'] != 'Пусто')
+                                                <a href="{{$returning['link']}}">Ссылка</a>
+                                            @else
+                                                <p>Заказ beauty</p>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
                         </tbody>
                         <tfoot>
                             <tr>
@@ -27,6 +71,7 @@
                                 <th>Сумма платежа</th>
                                 <th>Сайт</th>
                                 <th>Id заказа</th>
+                                <th>Ссылка</th>
                             </tr>
                         </tfoot>
                     </table>
@@ -53,11 +98,65 @@
     <script src="/assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
     <script>
-        $(function () {
-            $("#example1").DataTable({
-                "responsive": true, "lengthChange": false, "autoWidth": false,
-                "buttons": ["csv", "excel", "pdf", "print", "colvis"]
-            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        // $(function () {
+        //     $('#payment-table').DataTable({
+        //         "responsive": true, "lengthChange": false, "autoWidth": false,
+        //         "buttons": ["excel", "print"],
+        //         "paginate": {
+        //             "first": 'First page',
+        //             "next": 'Следующая',
+        //             "previous": 'Предыдущая'
+        //         }
+        //
+        //     })
+        // });
+        let table = $('#returning-table').DataTable({
+            buttons: [
+                'excel', 'print'
+            ],
+            order: [[0, 'desc']],
+            pageLength: 100,
+            lengthChange: false,
+            oLanguage: {
+                oPaginate: {
+                    sFirst: "Первая", // This is the link to the first page
+                    sPrevious: "Пред.", // This is the link to the previous page
+                    sNext: "След.", // This is the link to the next page
+                    sLast: "Последняя" // This is the link to the last page
+                }
+            }
+        });
+        table.buttons().container().appendTo('#returning-table_wrapper .col-md-6:first');
+        $('#returning-table_wrapper .col-md-6:first').addClass('d-flex align-items-center')
+        $('#returning-table_wrapper .col-md-6:nth-child(2)').addClass('d-flex align-items-center justify-content-end')
+    </script>
+
+    <script>
+        /* Локализация datepicker */
+        $.datepicker.regional['ru'] = {
+            closeText: 'Закрыть',
+            prevText: 'Предыдущий',
+            nextText: 'Следующий',
+            currentText: 'Сегодня',
+            monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+            monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+            dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+            dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+            dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+            weekHeader: 'Не',
+            dateFormat: 'dd.mm.yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['ru']);
+    </script>
+
+    <script>
+        $(function(){
+            $("#date_start").datepicker();
+            $("#date_end").datepicker();
         });
     </script>
 @endsection
